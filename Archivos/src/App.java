02432f157a94f656683139dbdfe0b0258e44baa1
Alex_Figueroa <trance4life503@gmail.com>
2021-01-16 00:25:02 -0600

    borrar registro

--- Archivos/src/App.java
+++ Archivos/src/App.java
@@ -1,23 +1,63 @@
+import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) {
 
         Control ctr = new Control();
+        int opcion;
 
         String path = "registro.txt";
+      
 
-        /*
-         * String id, name, age;
-         * 
-         * ctr.crearArchivo(path);
-         * 
-         * id = "0078"; name = "Rodrigo"; age = "20";
-         * 
-         * ctr.escribirArchivo(id, name, age);
-         * 
-         */
-        
-       
+        String ID, name, age;
+
+        opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
+                "Que opcion desea\n1. Crear archivo\n2. escribir\n3. leer\n4. limpiar\n.5 borrar\n6. eliminar registro\n Otro numero para salir"));
+
+        while (opcion >= 1 && opcion <= 6) {
+
+            switch (opcion) {
+                case 1:
+
+                    ctr.crearArchivo(path);
+
+                    break;
+
+                case 2:
+
+                    ID = JOptionPane.showInputDialog(null, "Ingrese un Id ejemplo 0001");
+                    name = JOptionPane.showInputDialog(null, "Ingrese un nombre");
+                    age = JOptionPane.showInputDialog(null, "Ingrese una edad");
+                    ctr.escribirArchivo(ID, name, age);
+
+                    break;
+                case 3:
+
+                ctr.leerTexto(path);
+
+                    break;
+                case 4:
+
+                    ctr.limpiarArchivo(path);
+
+                    break;
+                case 5:
+
+                ctr.eliminarArchivo(path);
+                    break;
+                case 6:
+
+               String delete =  JOptionPane.showInputDialog(null, "ID del registro que desea eliminar");
+                ctr.borrarRegistro(path, delete);
+                    break;
+            }
+
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "Que opcion desea\n1. Crear archivo\n2. escribir\n3. leer\n4. limpiar\n.5 borrar\n6. eliminar registro\n Otro numero para salir"));
+
+        }
 
     }
 }
+
+
--- Archivos/src/Control.java
+++ Archivos/src/Control.java
@@ -1,19 +1,24 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class Control {
 
     private File file;
     private String path, ID, name, age;
+    private Scanner x;
 
     public void crearArchivo(String path) {
 
         this.path = path;
+
         file = new File(path);
 
         try {
@@ -40,9 +45,14 @@ public class Control {
         try {
 
             FileWriter write = new FileWriter(file, true);
+            BufferedWriter bw = new BufferedWriter(write);
+            PrintWriter pwritter = new PrintWriter(bw);
 
-            write.write(ID + "," + name + "," + age + "\r" + "\n");
-            write.close();
+            pwritter.println(ID + "," + name + "," + age);
+           // write.write(ID + "," + name + "," + age + "\r" + "\n");
+            pwritter.flush();
+            pwritter.close();
+            bw.close();
 
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, e);
@@ -51,15 +61,17 @@ public class Control {
     }
 
     public void leerTexto(String path) {
-
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_GREEN = "\u001B[32m";
         String readLine;
         try {
             FileReader reader = new FileReader(path);
             BufferedReader breader = new BufferedReader(reader);
 
             readLine = breader.readLine();
+            System.out.println(ANSI_RED + "Lista de registros\n");
             while (readLine != null) {
-                System.out.println(readLine);
+                System.out.println(ANSI_GREEN + readLine);
                 readLine = breader.readLine();
             }
 
@@ -72,7 +84,6 @@ public class Control {
         }
 
     }
-
 
     public void eliminarArchivo(String path) {
 
@@ -87,6 +98,122 @@ public class Control {
         }
 
     }
+
+    public void limpiarArchivo(String path) {
+
+        this.path = path;
+        FileWriter writer;
+        try {
+            writer = new FileWriter(path);
+            writer.write("");
+            writer.close();
+        } catch (IOException e) {
+
+        }
+
+    }
+
+    /*public void borrarRegistro(String path, String delete) {
+
+        this.path = path;
+        String tempfile = "tempfile.txt";
+        File oldfile = new File(path);
+        File newfile = new File(tempfile);
+        File temp = new File(path);
+
+        name = "";
+        age = "";
+        ID = "";
+
+        try {
+
+            FileWriter writter = new FileWriter(tempfile);
+            BufferedWriter bwritter = new BufferedWriter(writter);
+            PrintWriter pwritter = new PrintWriter(bwritter);
+
+            File tm = new File(path);
+            try {
+                x = new Scanner(tm);
+                x.useDelimiter("[,]");
+
+                while (x.hasNextLine()) {
+
+                    ID = x.next();
+                    name = x.next();
+                    age = x.next();
+
+                    if (!ID.equals(delete)) {
+
+                        pwritter.println(ID + "," + name + "," + age);
+                    }
+
+                }
+
+                x.close();
+                bwritter.flush();
+                bwritter.close();
+                pwritter.flush();
+                pwritter.close();
+
+                oldfile.delete();
+                newfile.renameTo(temp);
+
+            } catch (FileNotFoundException ex) {
+
+            }
+
+        } catch (IOException e) {
+
+        }
+
+    }*/
+
+    public void borrarRegistro(String path, String delete) {
+
+        File file = new File(path);
+        String tempfile = "tempfile.txt";
+        File temp = new File(tempfile);
+        String name = "", age = "", ID = "";
     
+        try {
+    
+          FileWriter writter = new FileWriter(tempfile);
+          BufferedWriter bwritter = new BufferedWriter(writter);
+          PrintWriter pwritter = new PrintWriter(bwritter);
+    
+          try {
+            Scanner x = new Scanner(file);
+            x.useDelimiter("[,\n]");
+    
+            while (x.hasNext()) {
+    
+              ID = x.next();
+              name = x.next();
+              age = x.next();
+    
+              if (!ID.equals(delete)) {
+    
+                pwritter.println(ID + "," + name + "," + age);
+              }
+    
+            }
+    
+            x.close();
+            pwritter.flush();
+            pwritter.close();
+    
+            if (file.delete()) {
+              temp.renameTo(file);
+            }
+    
+          } catch (FileNotFoundException ex) {
+    
+          }
+    
+        } catch (IOException e) {
+    
+        }
+    
+      }
 
 }
